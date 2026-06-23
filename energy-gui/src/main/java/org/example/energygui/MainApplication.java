e123190ad7ce95c31efd237f6ac2bf32b614691b
biancagerstenbauer <wi25b037@technikum-wien.at>
2026-06-23 10:52:43 +0200

    Gui bearbeitet

--- energy-gui/src/main/java/org/example/energygui/MainApplication.java
+++ energy-gui/src/main/java/org/example/energygui/MainApplication.java
@@ -3,19 +3,13 @@ package org.example.energygui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("MainView.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setTitle("Energy Community Monitor");
         stage.setResizable(false);
